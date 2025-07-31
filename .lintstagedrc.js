9329a128b9410c45022b0a2953964c309477d6d5
MrAJBurns <147350793+MrAJBurns@users.noreply.github.com>
2025-07-30 21:36:56 -0700

    Initial commit

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
-  '*.java': ['./gradlew spotlessApply'],
-  '*.tf': ['terraform fmt'],
-};
